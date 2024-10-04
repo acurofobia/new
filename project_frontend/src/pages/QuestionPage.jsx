fbe35b50dc3a0b8cfbfaabde9fba0eb0be4ef8a7
acurofobia <acurofobia@gmail.com>
2024-10-04 15:21:52 +0500

    132123132

--- project_frontend/src/pages/QuestionPage.jsx
+++ project_frontend/src/pages/QuestionPage.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 import Question from '../components/Question';
 import ScrollToTop from '../components/ScrollToTop';
 import '../styles/questionPage.css'
+import CountdownTimer from '../components/CountdownTimer';
 
 const QuestionPage = () => {
   const {number} = useParams();
@@ -19,6 +20,7 @@ const QuestionPage = () => {
   return (
     <>
       <ScrollToTop></ScrollToTop>
+      <CountdownTimer setEnd={setEnd}></CountdownTimer>
       <div className='question-page-wrapper'>
         <Question key={number} number={number} setEnd={setEnd} arrayOfCheckedAnswers={arrayOfCheckedAnswers} setArrayOfCheckedAnswers={setArrayOfCheckedAnswers}></Question>
       </div>
